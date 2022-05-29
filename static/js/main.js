150e1e574b5ce99baeb7198d835b04182604fd95
Marcones Oliveira <marcones.oliveira.ifrn@gmail.com>
2022-05-29 20:21:15 -0300

    Correção de algumas referências e adição de áudio

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,20 @@
 const addClass = (divEvent) => {
     const element = document.querySelector(`#${divEvent}`);
     element.classList.add("animate__animated");
+    const audio = document.querySelector("#soundtrack");
+    audio.play();
 };
 const removeClass = (divEvent) => {
     const element = document.querySelector(`#${divEvent}`);
     element.classList.remove("animate__animated");
+    const audio = document.querySelector("#soundtrack");
+    audio.pause();
+    audio.currentTime = 0;
 };
 
 transition = (divEvent) => {
+    const audio = document.querySelector("audio");
+    audio.play();
     const element = document.querySelector(`#${divEvent}`);
     element.classList.replace("visually-hidden", "visible");
     element.scrollIntoView({ block: "start", behavior: "smooth" });
